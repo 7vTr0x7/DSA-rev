611186d28aa848ad717a59b7c1b8ef9a6f7c57e8
7vtr0x7 <7vtrox7@gmail.com>
2025-08-20 20:09:53 +0530

    pattern5

--- starPattern/pattern5.js
+++ starPattern/pattern5.js
@@ -1,7 +1,17 @@
-for (let i = 5; i >= 1; i--) {
+// for (let i = 5; i >= 1; i--) {
+//   let row = "";
+//   for (let j = 1; j <= i; j++) {
+//     row += j;
+//   }
+//   console.log(row);
+// }
+
+const n = 5;
+
+for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += j;
+  for (let j = 0; j < n - i; j++) {
+    row += j + 1;
   }
   console.log(row);
 }
