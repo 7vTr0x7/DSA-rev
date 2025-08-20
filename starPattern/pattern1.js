272c6066de6a72c8eb0a29c08990ae868cb659b2
7vtr0x7 <vtrox7@gamil.com>
2025-08-20 19:31:36 +0530

    pattern1

---
+++ starPattern/pattern1.js
@@ -0,0 +1,13 @@
+const n = 4;
+
+const printPattern = () => {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < n; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+};
+
+printPattern();
