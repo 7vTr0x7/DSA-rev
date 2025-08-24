5b4641ef8a38ab3850ad8c5d309533fd010b8430
7vtr0x7 <7vtrox7@gmail.com>
2025-08-24 20:01:25 +0530

    palindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -6,3 +6,17 @@
 
 // console.log(isPalindrome(number));
 // console.log(isPalindrome(121));
+
+const isPalindrome = (n) => {
+  let copy = n;
+  let rev = 0;
+
+  while (n > 0) {
+    let rem = n % 10;
+    rev = 10 * rev + rem;
+    n = Math.floor(n / 10);
+  }
+
+  return copy === rev ? true : false;
+};
+console.log(isPalindrome(1261));
