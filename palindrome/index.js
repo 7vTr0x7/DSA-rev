7b98454910860b3e4b83d79140637f8ada089781
7vtr0x7 <7vtrox7@gmail.com>
2025-08-24 19:31:14 +0530

    palindrome

---
+++ palindrome/index.js
@@ -0,0 +1,8 @@
+// let number = 144;
+
+// const isPalindrome = (number) => {
+//     return number == number.toString().split("").reverse().join("");
+// };
+
+// console.log(isPalindrome(number));
+// console.log(isPalindrome(121));
